9a33f2352440f145fc18e8efe5832d4a7b40f1d8
Luke Preston <lukejpreston@gmail.com>
2017-09-06 22:17:01 +0100

    whoops

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 
 class App extends Component {
   render () {
-    console.log(data)
+    // console.log(data)
     return (
       <div className='App'>
         <div className='App-header'>
